4553acda70f80d3d0942a901e28d48b50b902353
SAMUEL-UME <104633362+SAMUEL-UME@users.noreply.github.com>
2022-12-02 22:17:10 +0100

    Setting up cores

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+export const host = "https://beautiful-turtleneck-clam.cyclic.app";
+// "http://localhost:5000";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
