086808a53a7dcec07f9dc29ca2545bd129e0a598
Samuel Ume <cume357@gmail.com>
2023-02-15 23:02:22 +0100

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host ="http://localhost:5000";
+export const host ="https://snappy-backend-h585.onrender.com";
     // "https://beautiful-turtleneck-clam.cyclic.app";
 
 export const registerRoute = `${host}/api/auth/register`;
